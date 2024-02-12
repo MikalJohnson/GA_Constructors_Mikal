9eaeb999932397aff56a9b91f2c29f61ed457779
MikalJohnson <Johnsonmikal24@gmail.com>
2024-02-12 15:09:17 -0800

    Add project files.

---
+++ GA_Constructors_Mikal/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Constructors_Mikal
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+
+        // Custom constructor
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+        // Field initialization in constructors ensure objects have valid states
+        public Book(string title)
+        {
+            this.title = title;
+            this.author = "No Author Given"; // Default value
+        }
+    }
+}
---
+++ GA_Constructors_Mikal/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GA_Constructors_Mikal
+{
+    // Mikal Johnson
+    // Constructors purpose is to create and initialize
+    // objects. It ensure that it is setup properly
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Book newBook = new Book(); // <--- This is a default constructor
+            Book bookAuthorAndName = new Book("Night Watch", "Terry Pratchett");
+            Book bookNameOnly = new Book("Wyrd Sisters");
+
+            Book book1 = new Book("1984", "George Orwell");
+            Book book2 = new Book("The Hobbit");
+
+            Console.WriteLine(book1);
+        }
+    }
+}
